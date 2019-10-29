01a773c8e19152f4925ab30376bf5aac42097bf1
ALEEF02 <thesmarty2002@gmail.com>
2019-10-29 07:27:30 -0700

    Prettify

--- js/login.js
+++ js/login.js
@@ -1,33 +1,31 @@
 (function() {
+	const firebaseConfig = {
+		apiKey: "AIzaSyCBTFnvStVgUQ1FL1B9DosuqiCGwgGFe0A",
+		authDomain: "ocfa-group-notification-app.firebaseapp.com",
+		databaseURL: "https://ocfa-group-notification-app.firebaseio.com",
+		projectId: "ocfa-group-notification-app",
+		storageBucket: "ocfa-group-notification-app.appspot.com",
+		messagingSenderId: "11822023829",
+		appId: "1:11822023829:web:3ccbef5017154d6c68479c",
+		measurementId: "G-D2Y02FC2QH"
+	};
+	firebase.initializeApp(firebaseConfig);
 
- const firebaseConfig = {
-  apiKey: "AIzaSyCBTFnvStVgUQ1FL1B9DosuqiCGwgGFe0A",
-  authDomain: "ocfa-group-notification-app.firebaseapp.com",
-  databaseURL: "https://ocfa-group-notification-app.firebaseio.com",
-  projectId: "ocfa-group-notification-app",
-  storageBucket: "ocfa-group-notification-app.appspot.com",
-  messagingSenderId: "11822023829",
-  appId: "1:11822023829:web:3ccbef5017154d6c68479c",
-  measurementId: "G-D2Y02FC2QH"
- };
- firebase.initializeApp(firebaseConfig);
+	document.getElementById("submit").addEventListener('click', e => {
+		var email = document.getElementById("email").value;
+		var password = document.getElementById("password").value;
+		const auth = firebase.auth();
 
- document.getElementById("submit").addEventListener('click', e => {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  const auth = firebase.auth();
-
-  const promise = auth.signInWithEmailAndPassword(email, password);
-  promise.catch(e => console.log(e.message));
- });
- firebase.auth().onAuthStateChanged(firebaseUser => {
-  if (firebaseUser) {
-   window.location = "index.html";
-   console.log(firebaseUser);
-  } else {
-   console.log('Failed Login')
-  }
-
- });
-
+		const promise = auth.signInWithEmailAndPassword(email, password);
+		promise.catch(e => console.log(e.message));
+	});
+	
+	firebase.auth().onAuthStateChanged(firebaseUser => {
+		if (firebaseUser) {
+			window.location = "index.html";
+			console.log(firebaseUser);
+		} else {
+			console.log('Failed Login')
+		}
+	});
 }());
